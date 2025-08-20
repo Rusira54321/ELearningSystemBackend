6867332c99acae9473ee4306dfd2e680285f2c4b
Rusira54321 <rusiradinujaya57@gmail.com>
2025-08-20 19:48:18 +0530

    i add different port number

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 const userRouter = require("./router/UserRouter")
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8000
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`)   
 })
