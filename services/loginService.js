f540bf4efb17b280a9652d634902f52ed3401710
Vidhu <viduhettiarachchi@gmail.com>
2025-08-28 12:27:50 +0530

    Updated index.js and UserRouter, Created LoginController, LoginService

---
+++ services/loginService.js
@@ -0,0 +1,35 @@
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+const user = require("../model/User")
+require('dotenv').config()
+
+const loginUserService = async({email,password}) =>{
+    const existUser = await user.findOne({email});
+    if(!existUser) throw new Error("User not found !");
+
+    const isPasswordValid = await bcrypt.compare(password, existUser.password);
+    if(!isPasswordValid) throw new Error("Invalid Password !");
+
+    //JWT
+    const token = jwt.sign(
+        {id: existUser._id, role: existUser.role},
+        process.env.TOKEN_KEY,
+        {expiresIn: "1d"}
+    );
+
+    return{
+        message:"Succesfully logged in",
+        role: existUser.role,
+        token,
+    };
+};
+
+module.exports = {loginUserService}
+
+
+
+
+
+
+
+
